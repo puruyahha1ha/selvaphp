bf8eb7c1ac069ef85d68d060f0573f94c4e448b1
puruyahha1ha <135855038+puruyahha1ha@users.noreply.github.com>
2023-06-16 04:14:27 +0900

    Add files via upload

--- header_login.php
+++ header_login.php
@@ -1,7 +1,7 @@
 <header>
     <div class="link_header">
         <form action="top.php" method="get">
-            <p><?php if (!empty($_SESSION['name_sei'] && !empty($_SESSION['name_mei']))) echo "ようこそ" . $_SESSION['name_sei'] . $_SESSION['name_mei'] . "様" ?></p>
+            <?php if (!empty($_SESSION['name_sei'] && !empty($_SESSION['name_mei']))) echo "<p>ようこそ" . $_SESSION['name_sei'] . $_SESSION['name_mei'] . "様</p>" ?>
             <input type="submit" name="confirm" value="ログアウト" class="button_header">
         </form>
     </div>
