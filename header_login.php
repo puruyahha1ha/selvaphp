f836cc28e7037f3c0a358a602d9808e9b77e6b9a
puruyahha1ha <135855038+puruyahha1ha@users.noreply.github.com>
2023-06-16 02:32:43 +0900

    Add files via upload

--- header_login.php
+++ header_login.php
@@ -1,6 +1,6 @@
 <header>
         <div class="link_header">
-            <p><?php if (!empty($_SESSION['name_sei'] && !empty($_SESSION['name_mei'])))echo "ようこそ".$_SESSION['name_sei'].$_SESSION['name_nai']."様"?></p>
+            <p><?php if (!empty($_SESSION['name_sei'] && !empty($_SESSION['name_mei'])))echo "ようこそ".$_SESSION['name_sei'].$_SESSION['name_mei']."様"?></p>
             <a href="login.php">ログアウト</a>
         </div>
     </header>
--- login.php
+++ login.php
@@ -85,9 +85,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <form action="login.php" method="post">
             <div class="email">
                 <p>メールアドレス（ID）</p>
-                <input type="text" name="email" value="<?php if (!empty($_POST['email'])) {
-                                                            echo htmlspecialchars($_POST['email']);
-                                                        } ?>">
+                <input type="text" name="email" value="<?php if (!empty($_POST['email'])) {echo htmlspecialchars($_POST['email']);} ?>">
             </div>
             <div class="error">
                 <?php
@@ -101,9 +99,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </div>
             <div class="password">
                 <p>パスワード</p>
-                <input type="password" name="password" value="<?php if (!empty($_POST['password'])) {
-                                                                    echo htmlspecialchars($_POST['password']);
-                                                                } ?>">
+                <input type="password" name="password" value="<?php if (!empty($_POST['password'])) {echo htmlspecialchars($_POST['password']);} ?>">
             </div>
             <div class="error">
                 <?php
