8393964a1c369bb8c63291e21d0866732ed8cf74
puruyahha1ha <135855038+puruyahha1ha@users.noreply.github.com>
2023-07-03 02:38:55 +0900

    Add files via upload

--- thread_detail.php
+++ thread_detail.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 var_dump($_SESSION);
+var_dump($_POST);
 
 if (!empty($_GET['confirm']) && $_GET['confirm'] === 'スレッド一覧に戻る') {
     header('Location: thread.php', true, 307);
