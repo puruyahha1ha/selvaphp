47b4fe5e3902713726ee568ffa84b16a7176a3fc
puruyahha1ha <135855038+puruyahha1ha@users.noreply.github.com>
2023-07-03 17:44:51 +0900

    Add files via upload

--- thread_detail.php
+++ thread_detail.php
@@ -176,17 +176,16 @@ try {
 
                         $like = $prepare_like->fetch();
                         // いいねをしているかのチェック
-                        $prepare_check = $pdo->prepare("SELECT COUNT(member_id) FROM likes WHERE member_id = :member_id AND comment_id = :comment_id;");
-                        $prepare_check->bindValue(':member_id', $member_id, PDO::PARAM_INT);
+                        $prepare_check = $pdo->prepare("SELECT member_id FROM likes WHERE comment_id = :comment_id;");
                         $prepare_check->bindValue(':comment_id', $comment_id, PDO::PARAM_INT);
                         $prepare_check->execute();
-                        $check = $prepare_check->fetch();
+                        $check = $prepare_check->fetchAll();
 
                         var_dump($like, $member_id,$check);
 
                         echo "<div class='comment'>" . $number . ".　" . $val['name_sei'] . '　' . $val['name_mei'] . '　' . $val['created_at'] . '<br>';
                         echo nl2br(htmlspecialchars($val['comment'])) . "<br>";
-                        if ($check) {
+                        if (in_array($member_id, $check)) {
                             echo "   <div class='like'><a href='thread_detail.php?page_id={$now}&id={$id}&like=1&comment_id={$comment_id}'><img src='img\like.png'>";
                         } else {
                             echo " <div class='like'><a href='thread_detail.php?page_id={$now}&id={$id}&like=0&comment_id={$comment_id}'><img src='img\unlike.png'>";
