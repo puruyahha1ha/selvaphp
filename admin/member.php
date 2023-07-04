b43b7d788b1895c2e941ff70231c6e3d4a641df0
puruyahha1ha <135855038+puruyahha1ha@users.noreply.github.com>
2023-07-04 16:40:47 +0900

    Add files via upload

--- admin/member.php
+++ admin/member.php
@@ -48,10 +48,10 @@ try {
         var_dump($id,$_POST,$sql);
         $prepare = $pdo->prepare($sql);
         $prepare->bindValue(':id', $id, PDO::PARAM_INT);
-        $prepare->bindValue(':man', $man, PDO::PARAM_STR);
-        $prepare->bindValue(':woman', $woman, PDO::PARAM_STR);
-        $prepare->bindValue(':pref_name', $pref_name, PDO::PARAM_STR);
-        $prepare->bindValue(':free_word',  '%'.$free_word.'%', PDO::PARAM_STR);
+        // $prepare->bindValue(':man', $man, PDO::PARAM_STR);
+        // $prepare->bindValue(':woman', $woman, PDO::PARAM_STR);
+        // $prepare->bindValue(':pref_name', $pref_name, PDO::PARAM_STR);
+        // $prepare->bindValue(':free_word',  '%'.$free_word.'%', PDO::PARAM_STR);
         $prepare->execute();
         $records = $prepare->fetchAll();
         var_dump($prepare,$records);
