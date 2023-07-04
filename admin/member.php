df91dc33ca43e2051d71226d49506b4991138c6a
puruyahha1ha <135855038+puruyahha1ha@users.noreply.github.com>
2023-07-04 12:38:27 +0900

    Add files via upload

---
+++ admin/member.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://unpkg.com/modern-css-reset/dist/reset.min.css" />
+    <link rel="stylesheet" href="index.css">
+    <title>会員一覧</title>
+</head>
+
+<body>
+    <header>
+        <h2>会員一覧</h2>
+        <form action="top.php" action="get">
+            <input type="submit" name="confirm" value="トップへ戻る" class="button_header">
+        </form>
+    </header>
+
+</body>
+
+</html>
