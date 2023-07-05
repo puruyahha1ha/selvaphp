e59e76713e0fb70422fd14ef5836538f353825f8
puruyahha1ha <135855038+puruyahha1ha@users.noreply.github.com>
2023-07-05 17:35:15 +0900

    Add files via upload

--- admin/member.php
+++ admin/member.php
@@ -116,8 +116,8 @@ try {
         }
         if (isset($man) && isset($woman)) {
             $prepare->bindValue(':man', $man, PDO::PARAM_STR);
-            $prepare->bindValue(':woman', $man, PDO::PARAM_STR);
-            $count->bindValue(':man', $woman, PDO::PARAM_STR);
+            $prepare->bindValue(':woman', $woman, PDO::PARAM_STR);
+            $count->bindValue(':man', $man, PDO::PARAM_STR);
             $count->bindValue(':woman', $woman, PDO::PARAM_STR);
             var_dump("a");
         } elseif (isset($man) && empty($woman)) {
